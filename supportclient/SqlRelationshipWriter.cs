b1bd74113331744540c041075030fce10bc1b9ff
essentiaparfum <esentiaparfuminfo@gmail.com>
2025-04-29 16:53:55 +0000

    Sql kapcsolat létrehozása egy új csharp osztállyal

---
+++ supportclient/SqlRelationshipWriter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace supportclient
+{
+    public class SqlRelationshipWriter
+    {
+        private readonly string _connectionString;
+
+        public SqlRelationshipWriter(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void SaveRelationships(string productId, List<string> relatedProductIds)
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+
+                foreach (var relatedId in relatedProductIds)
+                {
+                    var cmd = new SqlCommand(@"
+                        INSERT INTO ProductRelationships 
+                        (ProductId, RelatedProductId, IsSubstitute, SortOrder, MarketingDescription, StoreId)
+                        VALUES 
+                        (@ProductId, @RelatedProductId, 0, 1, NULL, 1);
+                    ", conn);
+
+                    cmd.Parameters.AddWithValue("@ProductId", productId);
+                    cmd.Parameters.AddWithValue("@RelatedProductId", relatedId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
+
